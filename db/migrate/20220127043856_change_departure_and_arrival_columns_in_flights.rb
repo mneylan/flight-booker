b86adc53d1309f958dff17d284f120dc46f594a5
Michael <mineylan@gmail.com>
2022-05-30 20:25:50 -0700

    changed change dep and arriv migration for postgres

--- db/migrate/20220127043856_change_departure_and_arrival_columns_in_flights.rb
+++ db/migrate/20220127043856_change_departure_and_arrival_columns_in_flights.rb
@@ -1,8 +1,9 @@
 class ChangeDepartureAndArrivalColumnsInFlights < ActiveRecord::Migration[6.1]
   def change
+    change_column :flights, :departure_airport, 'integer USING CAST(departure_airport AS integer)'
+    change_column :flights, :arrival_airport, 'integer USING CAST(arrival_airport AS integer)'
+    
     change_table :flights do |t|
-      t.change :departure_airport, :integer
-      t.change :arrival_airport, :integer
       
       t.rename :departure_airport, :start_airport_id
       t.rename :arrival_airport, :end_airport_id
